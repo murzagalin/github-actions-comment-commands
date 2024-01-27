480faa7bfe736e3e8cca7f6b0e29a0cb3d8d3073
a.murzagalin <azamat.em@gmail.com>
2024-01-27 12:26:59 +0100

    failing and passing tests

---
+++ src/test/java/com/github/murzagalin/FailingTests.java
@@ -0,0 +1,13 @@
+package com.github.murzagalin;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class FailingTests {
+
+    @Test
+    void failingTest() {
+        assertEquals(1, 1 + 1);
+    }
+}
---
+++ src/test/java/com/github/murzagalin/PassingTests.java
@@ -0,0 +1,13 @@
+package com.github.murzagalin;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class PassingTests {
+
+    @Test
+    void passingTest() {
+        assertEquals(2, 1 + 1);
+    }
+}
